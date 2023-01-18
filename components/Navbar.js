60a795c113e6a25e468ac066517fd98dc457f81c
Alan Huynh <alanjhuynh@gmail.com>
2023-01-17 22:23:09 -0800

    Add Navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import { useSession, signOut } from 'next-auth/react';
+
+function Navbar() {
+    const {data: session } = useSession();
+
+    //TODO: update
+    if (!session)
+        return (
+            <div>loading</div>
+        )
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="d-flex justify-content-between align-items-center w-100 mx-5">
+                <Link href="/" className="text-white text-decoration-none">
+                    <h2>track<span className="text-muted">kilo</span></h2>
+                </Link>
+                <div className="dropdown">
+                    <a data-bs-toggle="dropdown">
+                        <img className="rounded-circle profile-pic" src={session.user.image ? session.user.image : "user.svg"}></img>
+                    </a>
+                    <div className="dropdown-menu dropdown-menu-end">
+                        <button onClick={() => signOut()} className="dropdown-item" type="button">Sign Out</button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,24 @@
-import '../css/form.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import '../css/global.css';
 import Head from 'next/head';
 import Link from 'next/link';
 import {SessionProvider} from 'next-auth/react'
 import { signOut } from 'next-auth/react';
+import Navbar from '../components/Navbar';
+import { useEffect } from "react";
 
 function App({ Component, pageProps, session }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
   return (
     <SessionProvider session={session}>
       <Head>
         <title>track kilo</title>
       </Head>
 
+      <Navbar></Navbar>
       <div className="d-flex justify-content-between mx-2">
         <h1>track kilo</h1>
         <div>
