b69789a9c05f382650ce7ec728e0e5e7a51fb11b
Alan Huynh <alanjhuynh@gmail.com>
2023-02-23 22:38:51 -0800

    Add edit mode template to card, add on cancel, and add delete button

--- components/Card.js
+++ components/Card.js
@@ -1,27 +1,54 @@
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { cloneDeep, size, toNumber } from "lodash";
 
 export const MAX_SET_COUNT = 100;
 
 const Card = ({ lift, isNew = true }) => {
     const [editMode, setEditMode] = useState(false);
-    const [setCount, setSetCount] = useState(0)
-
-    const [liftForm, setLiftForm] = useState({
+    
+    const initialLiftForm = {
         userId: lift.userId,
         name: lift.name,
         set: lift.set,
         rep: lift.rep,
-        weight: lift.weight,
-        metric: lift.metric,
         note: lift.note,
-        date: lift.date,
-      })
+        date: new Date(lift.date).toISOString().substring(0, 10),
+    };
+    const initialSetForm = cloneDeep(lift.sets);
+    const [liftForm, setLiftForm] = useState(initialLiftForm);
+    const [setCount, setSetCount] = useState(0);
+    const [setForm, setSetForm] = useState(initialSetForm);
 
-      useEffect(() => {
-        if (isNew)
-            setEditMode(true);
-      }, []);
+    useEffect(() => {
+    if (isNew)
+        setEditMode(true);
+    setSetCount(size(setForm));
+    }, []);
+
+    const onCancel = () => {;
+        setLiftForm(initialLiftForm);
+        setSetForm(initialSetForm);
+        setSetCount(size(setForm));
+        setEditMode(false);
+    };
+
+    const postDelete = async (form) => {
+        try {
+        const res = await fetch(`/api/lifts/${lift._id}`, {
+            method: 'DELETE',
+
+        })
+        if (!res.ok) {
+            throw new Error(res.status);
+        }
+        else {
+            //TODO: refresh list
+        }
+        } catch (error) {
+        console.log('Failed to add lift');
+        }
+    }
 
     function toggleEditMode(){
         setEditMode(!editMode);
@@ -33,24 +60,72 @@ const Card = ({ lift, isNew = true }) => {
         let name = target.name
 
         if (name == 'set'){
+            // if reducing the amount of sets, delete the extra sets
+            if (value < setCount)
+            for (let i = setCount; i > value; i--){ 
+                delete setForm[i];
+            }
+            
             if (value > 100)
                 setSetCount(MAX_SET_COUNT);
             else
-                setSetCount(value); 
+                setSetCount(toNumber(value));
         }
     }
+
+    const setLift = (e) => {                
+        const target = e.target
+        const value = target.value
+        const name = target.name
+    
+        setLiftForm({
+          ...liftForm,
+          [name]: value,
+        })
+      }
+    
+      const setSet = (e, index) => {
+        let set = {}
+        if (!setForm[index]){
+            //TODO: build frontend models
+            set = {
+                userId: lift.userId,
+                index: index,
+                rep: '',
+                weight: '',
+                metric: 'lb',
+                rpe: '',
+            }
+        }
+        else
+            set = setForm[index];
+    
+        const target = e.target
+        const value = target.value
+        const name = target.name
+    
+        set[name] = value;
+        
+        setSetForm({
+          ...setForm,
+          [index]: set,
+        })
+      }
 
     if (editMode) {
         return (
             <>
-                <div className="card bg-dark">
+                <div className={editMode ? 'card bg-dark h-100' : 'card bg-dark'}>
                     <div className="card-body">
                         <div className = "flex-between-center my-2">
-                            <input 
+                            <input
                                 className="bg-dark w-50"
+                                type="text"
+                                name="name"
+                                onChange={setLift}
                                 placeholder="Lift name"
+                                value={liftForm.name}
                             ></input>
-                            <a className="edit-icon " onClick={() => toggleEditMode()}>Save</a>
                         </div>
                         <div className="flex-between-center mb-2">
                             <div className="d-flex align-items-center">
@@ -58,19 +133,30 @@ const Card = ({ lift, isNew = true }) => {
                                     className="bg-dark w-25"
                                     type="number"
                                     name="set"
-                                    placeholder="Set"
+                                    onChange={setLift}
                                     onBlur={handleSetChange}
+                                    placeholder="Set"
+                                    value={liftForm.set}
                                 ></input>
                                 <FontAwesomeIcon className="mx-2" icon="fa-solid fa-x" />
                                 <input 
                                     className="bg-dark w-25"
+                                    type="number"
+                                    name="rep"
+                                    onChange={setLift}
+                                    required
                                     placeholder="Rep"
+                                    value={liftForm.rep}
                                 ></input>
                             </div>
                             <div>
                                 <input 
                                     className="bg-dark"
                                     type="date"
+                                    name="date"
+                                    onChange={setLift}
+                                    required
+                                    value={liftForm.date}
                                 ></input>
                             </div>
                         </div>
@@ -83,27 +169,43 @@ const Card = ({ lift, isNew = true }) => {
                                     <input 
                                         className="weight-input bg-dark h-75"
                                         type="number"
+                                        name="weight"
+                                        onChange={(e) => {setSet(e, i+1)}}
                                         placeholder="Weight"
+                                        value={setForm[i+1].weight}
                                     ></input>
-                                    <select className="metric-input bg-dark text-muted h-75">
-                                        <option>lb</option>
-                                        <option>kg</option>
+                                    <select name="metric" onChange={(e) => {setSet(e, i+1)}} value={setForm[i+1].metric} className="metric-input bg-dark text-muted h-75">
+                                        <option value="lb">lb</option>
+                                        <option value="kg">kg</option>
                                     </select>
                                 </div>  
                                 <div className="col-5 d-flex align-items-center">
-                                     <input 
+                                        <input 
                                         className="bg-dark h-75"
                                         type="number"
+                                        name="rep"
+                                        onChange={(e) => {setSet(e, i+1)}}
                                         placeholder="Rep"
+                                        value={setForm[i+1].rep}
                                     ></input>
                                     <input 
                                         className="bg-dark h-75"
                                         type="number"
+                                        name="rpe"
+                                        onChange={(e) => {setSet(e, i+1)}}
                                         placeholder="RPE"
+                                        value={setForm[i+1].rpe}
                                     ></input>
                                 </div>
                             </div>
                         ))}
+                        <div className="flex-between-center mt-4">
+                            <button className="btn bg-light" onClick={postDelete}>Delete</button>
+                            <span>
+                                <button className="btn bg-secondary" onClick={onCancel}>Cancel</button>
+                                <button className="btn bg-primary-2 ms-2" onClick={() => toggleEditMode()}>Save</button>
+                            </span>
+                        </div>
                     </div>
                 </div>
             </>
